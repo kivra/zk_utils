a764789f4b50ef68ed7a156aa890a8e673231350
Hakan Nilsson <hakan.nilsson@kivra.com>
2017-09-21 22:41:11 +0200

    Use erlzk instead of ezk

--- src/zk_utils_queue.erl
+++ src/zk_utils_queue.erl
@@ -20,36 +20,40 @@
 
 %%%_* Code =============================================================
 %%%_ * API -------------------------------------------------------------
+-spec enqueue(term(), string(), pid()) -> ok.
 enqueue(Term, Queue, Pid) ->
-  {ok, _} = ezk:create(Pid, Queue ++ "_", term_to_binary(Term), s),
+  {ok, _} = erlzk:create(Pid, Queue ++ "_", term_to_binary(Term),
+                         persistent_sequential),
   ok.
 
+-spec dequeue(string(), pid()) -> {ok, term()} | {error, empty}.
 dequeue(Queue, Pid) ->
   case seqnos(Queue, Pid) of
     [Seqno|_] ->
       EntryPath           = Queue ++ "_" ++ Seqno,
-      {ok, {TermBin, _}}  = ezk:get(Pid, EntryPath),
-      {ok, _}             = ezk:delete(Pid, EntryPath),
+      {ok, {TermBin, _}}  = erlzk:get_data(Pid, EntryPath),
+      ok                  = erlzk:delete(Pid, EntryPath),
       {ok, ?b2t(TermBin)};
     [] ->
       {error, empty}
   end.
 
+-spec to_list(string(), pid()) -> [term()].
 to_list(Queue, Pid) ->
   lists:map(fun(Seqno) ->
                 EntryPath = Queue ++ "_" ++ Seqno,
-                {ok, {TermBin, _}} = ezk:get(Pid, EntryPath),
+                {ok, {TermBin, _}} = erlzk:get_data(Pid, EntryPath),
                 binary_to_term(TermBin)
             end, seqnos(Queue, Pid)).
 
 %%%_ * Internals  ------------------------------------------------------
 seqnos(Queue, Pid) ->
-  {ok, List} = ezk:ls(Pid, filename:dirname(Queue)),
+  {ok, List} = erlzk:get_children(Pid, filename:dirname(Queue)),
   QueueName  = filename:basename(Queue),
   Seqnos     = lists:filtermap(
                  fun(File) ->
                      %% TODO: support '_' in queuename
-                     case string:tokens(?b2l(File), "_") of
+                     case string:tokens(File, "_") of
                        [QueueName, Seqno] -> {true, Seqno};
                        [_,         _    ] -> false %something else
                      end
